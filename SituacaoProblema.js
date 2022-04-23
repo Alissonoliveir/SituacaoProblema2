f73a2640e4213c001bd764c4a34f6745ceffe52c
Alissonoliveir <102183299+Alissonoliveir@users.noreply.github.com>
2022-04-22 22:08:55 -0300

    Esse sistema terá como função principal a divulgação dos eventos para a comunidade de desenvolvimento, para que sejam consultados de forma eficiente

---
+++ SituacaoProblema.js
@@ -0,0 +1,33 @@
+let dataAtual = 22/04/2022;
+let dataEvento = 25/04/2022;
+
+if (dataEvento >= dataAtual){
+console.log("data permitida");
+
+}else {
+    console.log("data invalidada");
+}
+let idadeDoParticipante = 18;
+if (idadeDoParticipante >=18){
+    console.log("idade permitida");
+}else{
+    console.log("idade nao permitida");
+}
+let Participantes = 4;
+if (Participantes <=100){
+console.log("cadastro permitido, limite não excedido");
+}else {
+    console.log("cadastro não permitido, limite excediddo");
+}
+
+listaParticipantes = ["Helena","Chico","Mario","Jose"];
+listaPalestrantes = ["Joao","Maria"];
+
+console.log("Inscritos: " + listaParticipantes);
+console.log("Palestrantes: " + listaPalestrantes);
+
+
+
+
+
+
